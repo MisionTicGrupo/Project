228d56d320740f5dc57aaf25cbd389b56c1b7db4
iLalaDev <develope.luis@gmail.com>
2022-09-13 20:57:40 -0500

    Backend base

--- ferreapp/models.py
+++ ferreapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.TextField()
+    descripcion = models.TextField()
+    ventas = models.IntegerField()
+    
+    precio_compra = models.IntegerField()
+    precio_venta = models.IntegerField()
+    
+    stock_inicial = models.IntegerField()
+    stock_disponible = models.IntegerField()
+    
+class Carrito(models.Model): # Carrito de compra (añadir, eliminar, comprar)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
--- ferreapp/views.py
+++ ferreapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def mainView(request):
+    return render(request, 'main.html', {
+        'form': UserCreationForm
+    })
+
+def loginView(request):
+    
+    if(request.method == "POST"):
+        print(request.POST)
+    
+    return render(request, 'login.html', {
+        'form': UserCreationForm
+    })
+    
+def registerView(request):
+    return render(request, 'register.html')
+
+def tasksView(request):
+    return render(request, 'tasks.html')
